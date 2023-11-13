8371b6750a4a619963e97acef02e25c51672f408
Nithya-Princy <141929247+Nithya-Princy@users.noreply.github.com>
2023-11-13 21:47:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  shortHex = shortHex.replace("#", "");
+
+  // Ensure that the input is a valid short hex code
+  if (shortHex.length !== 3 || !/^[0-9a-fA-F]{3}$/.test(shortHex)) {
+    console.error("Invalid short hex code. Please provide a valid 3-character hex code.");
+    return null;
+  }
+
+  // Expand each component to create the full hex code
+  const fullHex = shortHex
+    .split("")
+    .map(component => component.repeat(2))
+    .join("");
+
+  // Add the "#" symbol and return the result
+  return `#${fullHex}`;
 };
 
 // Do not change the code below.
